04153f49d360d30359be81656f92d2d985a11015
Marina Pamparas <marpamparas@gmail.com>
2024-05-29 17:40:25 -0300

    termine con views de cart y products handlebars

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -2,15 +2,15 @@ import { Router } from "express";
 import { ProductManager } from '../dao/ProductManager.js';
 import  initSocket  from '../sockets.js';
 import { ProductManagerMongoDb } from "../dao/ProductManagerMongoDb.js";
-import { MessagesManager } from "../dao/MessagesManager.js";
 
-
+import { CartManagerMongoDb } from "../dao/CartManagerMongoDb.js";
+import modelProducts from '../dao/models/products.models.js';
 
 
 const views = Router();
 //const PME = new ProductManager ("Products.json")
 const PMMDB = new ProductManagerMongoDb()
-
+const CMMDB = new CartManagerMongoDb ()
 
 
 const socket = initSocket();
@@ -60,14 +60,48 @@ views.get('/chat', async (req,res)=>{
 
 views.get('/products', async (req,res)=>{
 
-    try{
+    const options = {
+        page: parseInt(req.query.page) || 1,
+        limit: parseInt (req.query.limit) || 5,
+        lean: true
+    };
 
-        const productsFile = await PMMDB.getAllProducts()
-        const data = {data : productsFile}
-        res.status(200).render('home', data)
+    const query = {};
+
+    try{
+        const products = await modelProducts.paginate(query, options);
+
+        res.status(200).render('products', {
+            products: products.docs,
+            totalPages: products.totalPages,
+            currentPage: options.page,
+            showPrev: options.page > 1,
+            showNext: options.page < products.totalPages,
+            prevPage: options.page > 1 ? options.page - 1 : null,
+            nextPage: options.page < products.totalPages ? options.page + 1 : null
+
+        });
     
     }catch (error){
         console.error('Error al leer el archivo de productos:', error);
+        res.status(500).send('Error del servidor');
+    }
+});
+
+
+views.get('/cart/:cid', async (req,res)=>{
+
+    try{
+          
+        const cid = req.params.cid;
+        let cartProducts = await CMMDB.getCartById(cid);
+        cartProducts = JSON.parse(JSON.stringify(cartProducts)); // Elimina propiedades del prototipo porque sino handlebars por seguridad no las muestra
+        const data = { cart: cartProducts };
+        res.status(200).render('cart', data);
+
+
+    }catch (error){
+        console.error('Error al recuperar el carrito:', error);
         res.status(500).send('Error del servidor');
     }
 });
